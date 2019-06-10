791ddd1ce08f2075cc419dc57f399479ee00e582
Caidan Williams <caidanstevenwilliams@gmail.com>
2019-06-10 17:23:51 -0600

    updated singleton

--- src/main/java/com/mildmelon/designpatterns/creational/singleton/Singleton.java
+++ src/main/java/com/mildmelon/designpatterns/creational/singleton/Singleton.java
@@ -3,14 +3,33 @@ package com.mildmelon.designpatterns.creational.singleton;
 public class Singleton
 {
 
-    // Single static instance
-    private static Singleton instance = new Singleton();
+    // Single instance
+    private static volatile Singleton instance = null;
 
     // Private constructor so no one can use the keyword "new"
-    private Singleton() {}
+    private Singleton()
+    {
+        // Protect from instantiation on reflection
+        if (instance != null)
+        {
+            throw new RuntimeException("Use getInstance() method to create");
+        }
+    }
 
     public static Singleton getInstance()
     {
+        // Lazy load by only creating an instance when it's needed
+        if (instance == null)
+        {
+            // Don't create new instances for each thread
+            synchronized (Singleton.class)
+            {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+
         return instance;
     }
 
